e5a61a5aeb3f7fbc978dc14a3a7c5d08875cc5f2
Berk Hüseyin Gaffaroglu <berk.gaffaroglu@gmail.com>
2020-05-06 18:19:41 +0300

    heroku

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 app.config['SECRET_KEY'] = '1250501974'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
